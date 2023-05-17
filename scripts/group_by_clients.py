700673f727b3c661d42241a4f5f7cceeb78c9b72
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2023-05-17 17:23:13 +0200

    suppression de scripts inutiles et mise à jour de notebooks

--- scripts/group_by_clients.py
+++ scripts/group_by_clients.py
@@ -5,13 +5,4 @@ def group_by_clients(df):
     clients = [
         df.groupby("id_client").get_group(i).copy() for i in df["id_client"].unique()
     ]
-
-    # pour qu'on puisse traiter les jours facilements on change un peu le format :
-    """
-    reference_date = pd.to_datetime("2021-01-01")
-    for i in range(len(clients)):
-        clients[i]["jour"] = clients[i]["horodate"].map(
-            lambda x: (x - reference_date).days
-        )
-    """
     return clients
